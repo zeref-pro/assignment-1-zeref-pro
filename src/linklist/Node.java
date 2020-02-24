a9998bbed9250db5b1f3e2346eab45e8e27e0798
zeref-pro <ayush.chaturvedi_cs18@gla.ac.in>
2020-02-24 22:24:17 +0530

    Node class completed

--- src/linklist/Node.java
+++ src/linklist/Node.java
@@ -1,4 +1,31 @@
-package linklist;
 
-public class Node {
-}
+        package linklist;
+
+        import person.Person;
+
+        public class Node
+        {
+        private Person data;
+        private Node next;
+
+        Node(Person data)
+        {
+        this.data = data;
+        }
+
+        public Person getData() {
+        return data;
+        }
+
+        public void setData(Person data) {
+        this.data = data;
+        }
+
+        public Node getNext() {
+        return next;
+        }
+
+        public void setNext(Node next) {
+        this.next = next;
+        }
+        }
